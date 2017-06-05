a5046f7476648fc5606a9ebb8a410f224d49aa33
stephen <stephen.a.atwood@gmail.com>
2017-06-04 22:36:37 -0500

    Added test stuff to generate postScript Files

--- mst_test.go
+++ mst_test.go
@@ -1,44 +1,76 @@
 package delauney
 
 import (
+	"bufio"
+	"fmt"
 	"math/rand"
+	"os"
 	"testing"
-  "fmt"
 )
 
-func randomPoint(maxX, maxY float64) *Point {
+const folderName = "testFiles"
+
+func randomPoint(minX, maxX, minY, maxY float64) *Point {
+	var xRange = maxX - minX
+	var yRange = maxY - minY
 	return &Point{
-		X: maxX * r.Float64(),
-		Y: maxY * r.Float64(),
+		X: minX + xRange*r.Float64(),
+		Y: minY + yRange*r.Float64(),
 	}
 }
 
 var r *rand.Rand
 
-func TestPass(t *testing.T) {
-	r = rand.New(rand.NewSource(105))
-	N := 100
-	randomPoints := make([]*Point, N)
-	for n := range randomPoints {
-		randomPoints[n] = randomPoint(100, 100)
-		// fmt.Println(randomPoints[n])
-	}
+func TestMST99(t *testing.T) {
 
-	newMst := EuclideanMST(randomPoints)
-  if (len(newMst) != N-1) {
-    t.Fail()
-  }
 }
 
-func TestMSTEasy(t *testing.T) {
-	var points []*Point
-	for i := 0; i < 19; i++ {
-		for j := 0; j < 19; j++ {
-			points = append(points,
-        &Point{X: float64(5*i + 5), Y: float64(5*j + 5)})
-		}
+func TestMST1000(t *testing.T) {
+	r = rand.New(rand.NewSource(80))
+
+	N := 1000
+	minX := float64(0)
+	maxX := float64(300)
+	minY := float64(0)
+	maxY := float64(300)
+
+	randomPoints := make([]*Point, N)
+	for n := range randomPoints {
+		randomPoints[n] = randomPoint(minX, maxX, minY, maxY)
 	}
 
-	newMst := EuclideanMST(points)
-  fmt.Println(len(newMst))
+	drawPoints(randomPoints, minX, maxX, minY, maxY, "points1000.ps")
+}
+
+func totalWeight(edges []*Edge) float64 {
+	var result float64
+	for _, edge := range edges {
+		result += edge.length()
+	}
+	return result
+}
+
+func drawGraph(edges []*Edge) {
+
+}
+
+func drawPoints(points []*Point, minX, maxX, minY, maxY float64, fileName string) {
+	os.Mkdir(folderName, 0777)
+	f, err := os.Create(fmt.Sprintf("%s/%s", folderName, fileName))
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	w := bufio.NewWriter(f)
+
+  w.WriteString("%!PS-Adobe-2.0 EPSF-1.2\n")
+  w.WriteString(fmt.Sprintf("%%%%BoundingBox: %d %d %d %d\n", int(minX), int(minY), int(maxX), int(maxY)))
+  w.WriteString("%%EndComments\n")
+  w.WriteString("/drawO {newpath 0 360 arc stroke} def\n")
+
+  for _, point := range points {
+    w.WriteString(fmt.Sprintf("%f %f 1 drawO\n", point.X, point.Y))
+  }
+
+	w.Flush()
 }
